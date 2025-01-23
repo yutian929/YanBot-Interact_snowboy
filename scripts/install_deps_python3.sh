9d899155b2e8e6199e793b4fdeaff25c54f59369
yutian929 <yutian929@outlook.com>
2025-01-23 20:39:51 +0800

    add install bash scripts.

---
+++ scripts/install_deps_python3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Function to check command success
+check_success() {
+    if [ $? -ne 0 ]; then
+        echo "Error occurred in the previous command. Exiting."
+        exit 1
+    fi
+}
+
+# make snowboydetect
+cd swig/Python3/
+make
+check_success
+
+cd ../..
